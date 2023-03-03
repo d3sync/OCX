7ebed63211e72a7a866237bcb1852fcddef28a67
Alexander Patsouros <d3sync@users.noreply.github.com>
2023-03-03 10:39:25 +0200

    Update InstallOCX.ps1

--- InstallOCX.ps1
+++ InstallOCX.ps1
@@ -37,9 +37,12 @@ else {
 
 # Set the download link and local file path
 $downloadUrl = "http://datadesign.gr/Content/dist/OCX.zip"
+$fontsUrl = "http://datadesign.gr/Content/dist/fonts.zip"
 $filePath = "$env:USERPROFILE\Downloads\OCX.zip"
+$fontsPath = "$env:USERPROFILE\Downloads\fonts.zip"
 
 Start-BitsTransfer -Source $downloadUrl -Destination $filePath
+Start-BitsTransfer -Source $fontsUrl -Destination $fontsPath
 
 # Check if the file was downloaded successfully
 if (-not (Test-Path $filePath)) {
@@ -50,6 +53,7 @@ if (-not (Test-Path $filePath)) {
 
 # Extract the file to a folder with the same name as the file
 $extractPath = "$env:USERPROFILE\Downloads\OCX\"
+$extractPath2 = "$env:USERPROFILE\Downloads\fonts\"
 
 if (Test-Path $extractPath) {
    
@@ -61,12 +65,39 @@ else
   
     #PowerShell Create directory if not exists
     New-Item $extractPath -ItemType Directory
-    Write-Host "Folder Created successfully"
-
+    Write-Host "Folder $extractPath Created successfully"
 }
+
+if (Test-Path $extractPath2) {
+   
+    Write-Host "Folder Exists"
+    # Perform Delete file from folder operation
+}
+else
+{
+  
+    #PowerShell Create directory if not exists
+    New-Item $extractPath2 -ItemType Directory
+    Write-Host "Folder $extractPath2 Created successfully"
+}
+######## OCX ########
 Write-Host "Attempting to expand from $filepath to $extractPath"
 try {
 	Expand-Archive -Path $filePath -DestinationPath $extractPath
+    } catch {
+    Write-Host "Extraction failed. Trying alternative method..."
+    	if (Get-Module -ListAvailable -Name 7Zip4PowerShell) {
+    		Write-Host "The 7Zip4PowerShell module is installed."
+	} else {
+    		Write-Host "The 7Zip4PowerShell module is not installed."
+		Install-Module 7Zip4PowerShell -Scope CurrentUser -Force -Verbose
+	}    
+	Expand-7Zip -ArchiveFileName $filePath -TargetPath $extractPath -Verbose
+}
+######## FONTS ########
+Write-Host "Attempting to expand from $fontsPath to $extractPath2"
+try {
+	Expand-Archive -Path $fontsPath -DestinationPath $extractPath2
     } catch {
     Write-Host "Extraction failed. Trying alternative method..."
     # Your alternative extraction method here
@@ -76,11 +107,10 @@ try {
     		Write-Host "The 7Zip4PowerShell module is not installed."
 		Install-Module 7Zip4PowerShell -Scope CurrentUser -Force -Verbose
 	}    
-	Expand-7Zip -ArchiveFileName $filePath -TargetPath $extractPath -Verbose
+	Expand-7Zip -ArchiveFileName $fontsPath -TargetPath $extractPath2 -Verbose
 }
-
 # Output a message indicating success
-Write-Host "File downloaded and extracted successfully."
+Write-Host "Files downloaded and extracted successfully."
 
 # Set the source and destination folder paths
 $sourcePath = "$env:USERPROFILE\Downloads\OCX\"
@@ -93,7 +123,6 @@ $files = Get-ChildItem -Path $sourcePath -Filter "*.OCX"
 foreach ($file in $files) {
     Copy-Item -Path $file.FullName -Destination $destPath
 }
-
 # Register the copied files using regsvr32.exe
 foreach ($file in $files) {
     $regsvr32Path = Join-Path $env:SystemRoot "SysWOW64\regsvr32.exe"
@@ -102,11 +131,28 @@ foreach ($file in $files) {
 	Write-Host "Registered: $file"
 }
 
+######## Installing fonts ########
+$fonts = Get-ChildItem -Path $extractPath2 -Filter "*.ttf"
+Write-Host "Installing Fonts"
+foreach ($fontFile in $fonts) {
+    $fontFilePath = $fontFile.FullName
+    $fontDestinationPath = "C:\Windows\Fonts\" + $fontFile.Name
+    New-Item -ItemType File $fontDestinationPath -Force | Out-Null
+    Copy-Item $fontFilePath $fontDestinationPath -Force
+    Invoke-Item $fontDestinationPath
+}
+Write-Host "Done installing fonts!"
+########################################
 # Display a message for success
 Write-Host "All files copied and registered successfully."
+######## Deleting not needed OCX ########
 Write-Host "Deleting archive: $filePath"
 Remove-Item -Path $filePath -Recurse -Force
 Write-Host "Deleting Folder: $extractPath"
 Remove-Item -Path $extractPath -Recurse -Force
-
+######## Deleting not needed fonts ########
+Write-Host "Deleting archive: $filePath"
+Remove-Item -Path $fontsPath -Recurse -Force
+Write-Host "Deleting Folder: $extractPath2"
+Remove-Item -Path $extractPath2 -Recurse -Force
 Write-Host "All Done."
